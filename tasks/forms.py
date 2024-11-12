6476f81697104b6d31e74ced67b051269bd6149a
LAlit255 <150665941+LAlit255@users.noreply.github.com>
2024-11-12 15:09:55 +0530

    Update forms.py

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,15 +1,9 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 from .models import Task
+from django import forms
+from django.contrib.auth import authenticate
 
-# User Registration Form
-# class RegistrationForm(UserCreationForm):
-#     email = forms.EmailField()
-
-#     class Meta:
-#         model = User
-#         fields = ['username', 'email', 'password1', 'password2']
 
 from django import forms
 from django.contrib.auth.models import User
@@ -39,14 +33,6 @@ class RegistrationForm(forms.Form):
         user = User.objects.create_user(username=username, email=email, password=password)
         return user
 
-
-# User Login Form
-# class LoginForm(forms.Form):
-#     username = forms.CharField()
-#     password = forms.CharField(widget=forms.PasswordInput)
-
-from django import forms
-from django.contrib.auth import authenticate
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=150, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your username'}))
